948ef9721334b32b4cffddbb7af39f575b3ff080
codewithkate <kaitlencrawford@gmail.com>
2026-06-15 10:40:31 -0400

    clean up repo

--- models/staging/letterboxd/stg_letterboxd__diary.sql
+++ models/staging/letterboxd/stg_letterboxd__diary.sql
@@ -1,41 +1,24 @@
-with 
+with source as (
 
-diary as (
-
-    select * from {{ ref('base_letterboxd__diary') }}
+    select * from {{ source('letterboxd', 'diary') }}
 
 ),
 
-reviews as (
-
-    select * from {{ ref('base_letterboxd__reviews') }}
-
-),
-
-
-joined as (
+renamed as (
 
     select
-        d.entry_date
-        , d.title
-        , d.release_year
-        , d.letterboxd_uri
-        , d.rating
-        , d.rewatch_flag
-        , d.tags
-        , r.review
+        "c1" as entry_date
+        , "c2" as title
+        , "c3" as release_year
+        , "c4" as letterboxd_uri
+        , "c5" as rating
+        , "c6" as rewatch_flag
+        , "c7" as tags
+        , "c8" as watched_date
+        -- , "c9"
 
-    from diary d
-    left join reviews r
-        on r.entry = d.entry_date
-        and r.title = d.title
-        and r.release_year = d.release_year
-        and r.letterboxd_uri = d.letterboxd_uri
-        and r.rating = d.rating
-        and r.rewatch = d.rewatch_flag
-        and r.tags = d.tags
-        and r.watched_date = d.watched_date
+    from source
 
 )
 
-select * from joined
+select * from renamed
